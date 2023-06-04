520efd8983a8f438eb83644ea6216cd74938f462
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-06-04 21:28:11 +0300

    created a new file: controller

---
+++ python_project/controller.py
@@ -0,0 +1,27 @@
+import service as s
+import view as v
+
+
+def run():
+    input_from_user = ''
+    while input_from_user != '7':
+        v.start_board()
+        input_from_user = input().strip()
+        if input_from_user == '1':
+            s.show('all')
+        if input_from_user == '2':
+            s.add()
+        if input_from_user == '3':
+            s.show('all')
+            s.id_edit_del_show('del')
+        if input_from_user == '4':
+            s.show('all')
+            s.id_edit_del_show('edit')
+        if input_from_user == '5':
+            s.show('date')
+        if input_from_user == '6':
+            s.show('id')
+            s.id_edit_del_show('show')
+        if input_from_user == '7':
+            v.exit()
+            break
